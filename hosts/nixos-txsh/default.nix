3272c3f12802bdbd0937e948fb02d09e52550bd2
Hugefiver <i@iruri.moe>
2024-11-15 16:04:21 +0800

    nixos: remove efi support

--- hosts/nixos-txsh/default.nix
+++ hosts/nixos-txsh/default.nix
@@ -48,9 +48,9 @@ in {
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   boot.loader.grub.device = "/dev/vda"; # or "nodev" for efi only
   fileSystems."/".device = lib.mkDefault "/dev/vda";
