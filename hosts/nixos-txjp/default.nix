4e1cb6d44cebf376ae193000bc60db2ce81b0bf3
Hugefiver <i@iruri.moe>
2024-11-15 22:00:47 +0800

    nixos: enable efi

--- hosts/nixos-txjp/default.nix
+++ hosts/nixos-txjp/default.nix
@@ -45,13 +45,13 @@ in {
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
   # boot.loader.efi.efiSysMountPoint = "/boot/efi";
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "nixos-txsh"; # Define your hostname.
+  networking.hostName = "nixos-txjp"; # Define your hostname.
   networking.nameservers = [
     "1.0.0.1"
     "8.8.8.8"
