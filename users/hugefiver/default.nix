a342efe453c79f84d5ecc3f6e8fbfd7e279cf0bc
Hugefiver <i@iruri.moe>
2024-11-15 21:15:59 +0800

    nixos: add ssh pubkey

--- users/hugefiver/default.nix
+++ users/hugefiver/default.nix
@@ -3,5 +3,6 @@
   extraGroups = ["wheel"];
   openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+4SEP1ApMiuwdiBhcFa+/dy0fLc/Ekkqb8ZNxtEvMm hugefiver@LAPTOP-THB14P0"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtmS3WIgcG5+nVet6d5ZKIMAjTrncvelpkG57xT7UjR hugefiver@DESKTOP-D873LN7"
   ];
 }
