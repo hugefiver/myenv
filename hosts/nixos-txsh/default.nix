05c25d52a9784743331c3b0fb63d468bcd993fb0
Hugefiver <i@iruri.moe>
2024-11-22 20:58:07 +0800

    nixos: add xray

--- hosts/nixos-txsh/default.nix
+++ hosts/nixos-txsh/default.nix
@@ -141,6 +141,8 @@ in {
     wheelNeedsPassword = false;
   };
 
+  services.xray = { enable = true; settingsFile = "/etc/xray/config.json"; };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
