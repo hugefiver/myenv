137933b681749467e969d902b67bf0d30778e834
Hugefiver <i@iruri.moe>
2025-03-07 17:28:36 +0800

    nixos: add substituter

--- hosts/nixos-txsh/default.nix
+++ hosts/nixos-txsh/default.nix
@@ -39,6 +39,7 @@
 
   nix.settings.substituters = lib.mkForce [
     "https://mirror.sjtu.edu.cn/nix-channels/store"
+    "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
     # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
     # "https://mirrors.ustc.edu.cn/nix-channels/store"
     "https://cache.nixos.org"
