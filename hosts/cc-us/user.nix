2f19ee75ec3c57b1f1e0f863e7f6a2fba43ae38b
Hugefiver <i@iruri.moe>
2025-06-04 16:13:23 +0800

    nixos: update flake deps

--- hosts/cc-us/user.nix
+++ hosts/cc-us/user.nix
@@ -2,7 +2,6 @@
   users.users = rec {
     root = {
       openssh.authorizedKeys.keys = [] ++ hugefiver.openssh.authorizedKeys.keys;
-      hashedPassword = "$y$j9T$czocy.6VC6HhOTBn6DvtN0$vNIT4pDcYCjN4qJxgECELF5BZMsMpWlR8meO5M25Os4";
     };
     hugefiver = import "${self}/users/hugefiver";
     yan = import "${self}/users/yan";
