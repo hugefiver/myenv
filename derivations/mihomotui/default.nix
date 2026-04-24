025ab4eba237f019fbe9906781b35aa2e6bd20ab
Hugefiver <i@iruri.moe>
2026-04-24 18:54:47 +0800

    fix(mihomotui): pin vendorHash

--- derivations/mihomotui/default.nix
+++ derivations/mihomotui/default.nix
@@ -3,7 +3,7 @@ buildGoModule {
   pname = "mihomotui";
   version = "0.1.0";
   src = ./.;
-  vendorHash = lib.fakeHash;
+  vendorHash = "sha256-xa2wygHA1Yd+wIgayQYeNnPj7UjA/As9YCSzo85viAs=";
   meta = {
     description = "TUI for mihomo external-controller API";
     mainProgram = "mihomotui";
